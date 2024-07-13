b95dd9ad9456418f3c1f04da718fe97b9c68a502
LSG <sukyeng97@naver.com>
2024-07-13 15:39:50 +0900

    layout 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 
 function App() {
-<<<<<<< HEAD
   return (
     <div className="p-5">
       <Layout />
     </div>
   );
-=======
-  return <Layout />;
->>>>>>> 408743ced9164a03ba932ab19fc408d05c68f90f
 }
 
 export default App;
